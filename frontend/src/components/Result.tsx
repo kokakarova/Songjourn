b3589e67aad226568a90928b4c3c444e033c727f
kokakarova <kostadinka.karova@appliedtechnology.se>
2024-03-13 18:19:56 +0100

    format results page and add buttons to home and new quiz

--- frontend/src/components/Result.tsx
+++ frontend/src/components/Result.tsx
@@ -1,10 +1,18 @@
+import { Link } from "react-router-dom";
 import { result } from "./StartedQuiz";
 
 export default function Result() {
   return (
-    <>
-      <div>Well Played!</div>
-      <div>Your score is {result}/100</div>
-    </>
+    <div className="hero min-h-screen" style={{backgroundImage: 'url(eric-nopanen-8e0EHPUx3Mo-unsplash.jpg)'}}>
+  <div className="hero-overlay bg-opacity-60"></div>
+  <div className="hero-content text-center text-neutral-content">
+    <div className="max-w-md">
+      <h2 className="mb-5 text-5xl font-bold">Your score is {result}/100</h2>
+      <h1 className="mb-5">Thanks for playing</h1>
+      <button className="btn btn-primary m-1.5"><Link to="/">Home</Link></button>
+      <button className="btn btn-primary m-1.5"><Link to="/homequiz">New Quiz</Link></button>
+    </div>
+  </div>
+</div>
   );
 }
